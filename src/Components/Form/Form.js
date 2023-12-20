221882eff84c3df8c2b0bbb786c8b84c2accaf31
Lloyd Farnham <lloyd.farnham@crewclothing.co.uk>
2023-12-20 15:24:37 +0000

    tweaked min width functionality

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -141,29 +141,34 @@ function Form() {
   const generateOutputCode = () => {
     let tableClass = "wf";
     let overflow = "";
+    let minWidth = "";
 
     if (layout === "x1") {
       tableClass = "w50";
       setWidth(206);
+      minWidth = "min-width: 202px;";      
       overflow = "";
     } else if (layout === "x2") {
       tableClass = "wf";
       setWidth(206);
+      minWidth = "";   
       overflow = "";
     } else if (layout === "x3") {
       tableClass = "wf";
       setWidth(152);
-      overflow = "white-space: nowrap; overflow: hidden";
+      minWidth = "";      
+      overflow = "white-space: nowrap; overflow: hidden;";
     } else {
       tableClass = "wf";
       setWidth(418);
+      minWidth = "";      
       overflow = "";
     }
 
-    const code = `<table class="${tableClass}" align="center" border="0" cellspacing="0" cellpadding="0" role="presentation" width="${width}" style="width: ${width}px; min-width: ${width}px; background-color: #${fillColor}; border: 1.5pt solid #${strokeColor}">
+    const code = `<table class="${tableClass}" align="center" border="0" cellspacing="0" cellpadding="0" role="presentation" width="${width}" style="width: ${width}px; background-color: #${fillColor}; border: 1.5pt solid #${strokeColor}">
   <tr>
     <td class="button" align="center">
-      <a href="${url}" om:linkname="${linkName}" style="display: inline-block; font-weight: bold; color: #${textColor}; line-height: 35px; mso-padding-alt: 0; font-size: 12px; text-align: center; vertical-align: middle; text-decoration: none; width: 100%; ${overflow}" target="_blank"><!--[if mso]><i style="letter-spacing:20px;mso-font-width:-100%;mso-text-raise:20px;">&nbsp;</i><span style="mso-text-raise:10px;"><![endif]-->
+      <a href="${url}" om:linkname="${linkName}" style="display: inline-block; font-weight: bold; color: #${textColor}; line-height: 35px; mso-padding-alt: 0; font-size: 12px; text-align: center; vertical-align: middle; text-decoration: none; width: 100%; ${minWidth} ${overflow}" target="_blank"><!--[if mso]><i style="letter-spacing:20px;mso-font-width:-100%;mso-text-raise:20px;">&nbsp;</i><span style="mso-text-raise:10px;"><![endif]-->
         ${label}
         <!--[if mso]></span><i style="letter-spacing:20px;mso-font-width:-100%;">&nbsp;</i><![endif]--></a>
     </td>
